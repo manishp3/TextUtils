6a48ad0377d02decfb8916301c5cbd3552e1f70c
manishp3 <128165494+manishp3@users.noreply.github.com>
2023-09-27 08:09:33 +0530

    Add files via upload

---
+++ del.js
@@ -0,0 +1,11 @@
+function func() {
+	// Original string
+	let str = 'JavaScript is object oriented language';
+
+	// Finding the character at given index
+	let value = str.charAt(0:3);
+	// let value1 = str.charAt(8);
+	console.log(value);
+	// console.log(value1);
+}
+func();
